de620805415933145a4dc1814dbc589dcccb4032
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-27 12:34:03 +0300

    lesson 2 added

---
+++ lesson1b.js
@@ -0,0 +1,6 @@
+let weight = 10.5 // Declaring a variable with the initiallizer "let"
+const pi = 3.142  // Declaring a variable with the initiallizer "const" 
+
+// show output 
+console.log(weight)
+console.log(pi)
