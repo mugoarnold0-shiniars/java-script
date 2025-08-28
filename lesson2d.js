9c392cce33c5d470636085bdb2ab203d3244d4b3
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-28 12:41:25 +0300

    lesson 2d added

---
+++ lesson2d.js
@@ -0,0 +1,18 @@
+// iff...else if .... elese statement
+// it is used to evaluate mutiple condition
+
+let age=-2
+
+if (age < 15){
+    console.log ("You are too young to watch this episode")
+
+}
+else if (age>=15 && age<=18){
+    console.log ("You can watch this episode seek parental guidance")
+}
+else if (age>=18 && age<40){
+    onsole.log ("have a seat and enjoy this episode...")
+}
+else {
+    console.log("This movie doesn't suite you")
+}
