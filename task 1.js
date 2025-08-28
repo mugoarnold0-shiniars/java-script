cfb17df556775730e2b1cf9da3be8ea4f1e06df4
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-28 11:32:44 +0300

    task 1 added

---
+++ task 1.js
@@ -0,0 +1,14 @@
+let car ={
+    Type : "BMW",
+    Cost : 15000000,
+    isCarbofiber : true,
+    color  : "Lightblue"
+
+}
+console.log("The car description is:",car)
+console.log("THe colour of the car is:",car.color)
+
+
+let animals = ["Monkeys","Lions","Tortoise","Baboon","Panda","Giraffe","Rhinos"]
+console.log ("the animals in the zoo are",animals)
+console.log("The most rare animal is:",animals[4])
