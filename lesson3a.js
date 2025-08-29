c0f7b20143740546a630b5d20ca979a39ad659be
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-29 11:51:11 +0300

    lesson 3a added

---
+++ lesson3a.js
@@ -0,0 +1,28 @@
+// for loop
+// loops are used to iterate through something
+// Structure of the for loop;
+// 1. start value
+// 2. condition
+// 3. increment/decrement
+// 4. body ~The statement to be printed out
+
+
+// for (let i=0;i<=10;i++){
+//     console.log("the new value of i is:",i)
+// }
+
+// for (let i=1999;i<=2025;i++){
+        
+//         if (i%4==0 )
+//             console.log("is a leap year",i)
+//         else ;
+//             console.log("its not a leap year:",i)
+// }
+for (let i=10;i>=0;i--){
+    if (i/5==1){
+        console.log("the number is five",i)
+    }
+    else{
+        console.log(i)
+    }
+}
