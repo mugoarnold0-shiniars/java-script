2778b809b50450e1870133cb62481d2bcbbacf3b
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-29 12:07:53 +0300

    lesson3c  added

---
+++ lesson3c.js
@@ -0,0 +1,14 @@
+// map loop
+const numbers = [1,2,3,4,5]
+
+const double = numbers.map(num =>num*2)
+
+console.log(double)
+
+
+
+const names = ["James","David","Joyce","Alice","Ann"]
+
+const greeting = names.map(names=>"hello,"+ names)
+
+console.log(greeting)
