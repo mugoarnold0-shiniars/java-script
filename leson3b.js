687359cfecdeaec3e98ef0d20cb9054afd904e5d
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-08-29 12:00:10 +0300

    lesson 3b added

---
+++ leson3b.js
@@ -0,0 +1,20 @@
+// while loop
+// iy is commonly used when the length of yhe items is not known
+// structure of the while loop:
+// 1. inittialisation of a variable
+// 2. while keyword
+// 3.condition to be checked
+// 4. increment /decrement
+
+
+// let i=0
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+let i=10
+while (i>=1){
+    console.log(i);
+    i--;
+}
