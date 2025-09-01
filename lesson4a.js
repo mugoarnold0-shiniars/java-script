8c1dc220f3d3830544455d49bb73a70502a05707
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-01 11:28:29 +0300

    lesson 4a  added

---
+++ lesson4a.js
@@ -0,0 +1,16 @@
+// Javascript functions
+// Structure of a function
+// function keyword,name of the function,parenthesis followed by the block statement to be executed
+
+
+function sayHello(){
+    console.log("Hello there.Hope you are fine.Nice to meet you...")
+}
+// Call/Invfoke the function by its name
+sayHello();
+
+function sayGoodbye(){
+    console.log("Goodbye, see you later")
+
+}
+sayGoodbye()
--- task3.js
+++ task3.js
@@ -1,7 +1,13 @@
-let years= 1999
-while (years<=2021 ){
-    if (years%4==0){
-    console.log("ITS A LEAP YEAR",years)
+let years= 1692
+while (years<=1721 ){
+    if (years%400==0 ){
+            console.log("ITS A LEAP YEAR",years)
+    }
+else if (years%100==0){
+        console.log("ITS A LEAP YEAR",years)
+    }
+    else if (years%4==0){
+        console.log("ITS A LEAP YEAR",years)
     }
 else{
     console.log(years)
