2fc164144bfa88f05695fc57088e3c719c691849
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-01 12:23:46 +0300

    lesson4b  added

---
+++ lesson4b.js
@@ -0,0 +1,34 @@
+//  Function with parameters
+
+function greeting(name){
+    console.log("My name is",name,"how have you been?")
+
+}
+greeting("Arnold");
+
+function myCountry(country_name){
+    console.log("My favourite country is ",country_name)
+
+}
+myCountry("Kenya")
+myCountry("Morocco")
+myCountry("South Africa")
+
+
+function sum(a,b,c){
+  let total= a+b+c
+  console.log ("The sum is",total)
+
+}
+sum(3,87,90)
+sum(34,80,9)
+
+
+function SI(p,r,t){
+    let intrest= p*r*t/100
+    console.log("The simple intrest is:",intrest)
+
+}
+SI(20000,7,9)
+SI(40000,7.8,12)
+SI(170000,12,8)
