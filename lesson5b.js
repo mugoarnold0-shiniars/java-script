7fa87452cb57aebd2333e1488f0598096301bf62
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-02 11:18:27 +0300

    lesson5b  added

---
+++ lesson5b.js
@@ -0,0 +1,13 @@
+// Arrow functions
+
+const sayHello = ()=>{
+    console.log("This is an arrow function")
+}
+
+// to invoke it
+sayHello();
+
+const sayBye= ()=>{
+    console.log("Goodbye")
+}
+sayBye();
