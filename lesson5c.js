375f79b7baf8fadc80240766f1f7b1674fa12206
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-02 11:34:41 +0300

    lesson5c  added

---
+++ lesson5c.js
@@ -0,0 +1,15 @@
+// arrow function with parameters
+const greet=(jina)=>{
+    console.log("Hello",jina,"Umeshindaje?Natumaini umeshinda vyema.")
+}
+
+greet("Mary");
+greet("Andrew");
+
+// Bwelow is an arrow function to calculate the sum of two numbers
+const total = (a,b)=>{
+    let sum = a+b
+    console.log("The total of the two numbers is:",sum)
+}
+total(40,50);
+total(70,90);
