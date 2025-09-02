6f49de3145742cc7cfb504bc061884320cffd22f
mugoarnold0-shiniars <mugoarnold0@gmail.com>
2025-09-02 10:56:30 +0300

    lesson5a  added

---
+++ lesson5a.js
@@ -0,0 +1,12 @@
+// anonymus functions in java script
+const greet = function(){
+    console.log("Hello there.how are you?")
+}
+greet();
+
+
+// second example
+(function(){
+    console.log("Welcome to the world of programming")
+})
+();
